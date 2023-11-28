0c240368ce862b6987d6ca7ef3ddfb529a1df981
Erica  Lira <104800676+ericapaiva@users.noreply.github.com>
2023-11-28 19:28:14 +0000

    Atualização appts

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-/*
 import express, { Request, Response } from "express";
 
 const app = express();
@@ -6,10 +5,18 @@ const app = express();
 app.get("/", (req: Request, res: Response) => {
   res.send("Bem-vindo ao meu servidor!");
 });
+app.get("/usuario", (req: Request, res: Response) => {
+  const usuario = {
+    nome: "Danilo",
+    idade: 35,
+    email: "danilolirapersonal@hotmail.com",
+  };
+  res.json(usuario);
+});
 
 const porta = 3100;
+
 
 app.listen(porta, () => {
   console.log(`Servidor rodando em http://localhost:${porta}`);
 });
- */
